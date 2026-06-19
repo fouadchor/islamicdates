c9e0499246954c1ccc8e86cafabc91f5d79d7f94
Fouad <fouadchor@gmail.com>
2026-06-19 22:40:47 +0000

    Fix build: remove @astrojs/sitemap, restore static sitemap.xml with all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [react(), sitemap()],
+  integrations: [react()],
   output: 'static',
   site: 'https://islamicdates.org',
 });
